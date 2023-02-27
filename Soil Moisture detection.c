cc40a096d2862faf823e35cbdb27e0b4252de70d
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-02-27 14:27:49 +0530

    Create Soil Moisture detection.c

---
+++ Soil Moisture detection.c
@@ -0,0 +1,24 @@
+int sensorvalue = 0; // 0 means Soil having moisture
+int buzzer = 6;
+
+void setup() {
+  pinMode(10,INPUT);
+  Serial.begin(9600);
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  sensorvalue = digitalRead(10);
+  Serial.print('\n');
+  delay(1000);
+  
+  if(sensorvalue == 1){
+  Serial.println("Soil needs water");
+  delay(100);
+  }
+  else{
+    Serial.println("Soil is moisturized");
+    digitalWrite(buzzer,HIGH);
+      delay(100);
+  }
+}
