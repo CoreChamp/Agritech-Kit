1fab09fe2d39a3b5dcb9290e81b7aabe66a8b65a
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-03-03 10:08:10 +0530

    Create Rain Detection.c

---
+++ Serial Monitor/Rain Detection.c
@@ -0,0 +1,27 @@
+int sensorvalue = 0; 
+int buzzer = 6;
+
+void setup() {
+  pinMode(2,INPUT);
+  Serial.begin(9600);
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  sensorvalue = digitalRead(2);
+  Serial.print("Sensor value =");
+  Serial.print(sensorvalue);
+  Serial.print('\n');
+  delay(1000);
+  
+  if(sensorvalue == 1){
+  Serial.println("RAINING");
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  }
+  else{
+    Serial.println("NOT RAINING");
+    digitalWrite(buzzer,LOW);
+      delay(100);
+  }
+}
